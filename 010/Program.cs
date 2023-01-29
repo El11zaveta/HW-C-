58ee6916d01c5db5e849cd2724b84755a8fcbee4
Elizaveta <>
2023-01-29 13:36:30 +0300

    add new folder

---
+++ 010/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите 3-хзначное число");
+var n = Math.Abs(Convert.ToInt64(Console.ReadLine()));
+while (n >= 100)
+{
+    n /= 10;
+}
+var r = n % 10;
+Console.WriteLine(r);
