876583f0907ee4f8ca1dac3bdb887e4a95cd1738
Elizaveta <>
2023-01-29 14:10:34 +0300

    new ex

--- 013/Program.cs
+++ 013/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите любое число ");
+int n = int.Parse(Console.ReadLine()!);
+int result = 0;
+if (n >= 100)
+{
+    while (n > 999)
+    {
+        n = n / 10;
+    }
+    result = n % 10;
+}
+Console.WriteLine($"{result}");
