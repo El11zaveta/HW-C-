b911e55c92df736c815565f50527f060b9544846
Elizaveta <>
2023-02-06 17:40:56 +0300

    new ex

---
+++ ex2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N.
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine()!);
+int f = 1;
+int a = 1;
+
+while (a <= n)
+{
+    f = f * a;
+    Console.Write($"{f} ");
+    a++;
+}
+Console.WriteLine($" = {f}");
