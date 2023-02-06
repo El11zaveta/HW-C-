6257e4da65886e994ca5f8677ab15be7ede1248b
Elizaveta <>
2023-02-06 16:53:02 +0300

    add new ex

---
+++ ex1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитать сумму цифр в числе/
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.ReadLine()!);
+int n1 = n % 10;
+int sum = 0;
+
+while(n / 10 !=0)
+{
+    n = n / 10;
+    sum = sum + n % 10;
+}
+Console.WriteLine(sum + n1);
