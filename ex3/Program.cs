cea00c0f3f4de4e897ce53e26280bdad276768c3
Elizaveta <>
2023-02-06 17:56:25 +0300

    new ex

---
+++ ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.WriteLine("Введите число ");
+int n = int.Parse(Console.WriteLine()!);
+int a = 1;
+int cub = 1;
+
+while (a < n)
+{
+    cub = a * a * a;
+    if (cub % 2 == 0)
+    {
+        Console.Write($"{cub}");
+    }
+    a++;
+}
